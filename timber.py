7af913f2ca07434140e3472a4c7576ac78d6976c
David Emerson <d@nnix.com>
2021-04-01 23:57:06 -0400

    go!

--- timber.py
+++ timber.py
@@ -3,7 +3,7 @@ import time
 from time import ctime
 import os
 import configparser
-import requests
+from urllib.request import urlopen, Request
 
 def interval(seconds,t_i):
 	""" does the timing bit """
@@ -24,7 +24,7 @@ def interval(seconds,t_i):
 			now = time.time()
 			remaining = end - now
 			print(int(remaining)," remaining")
-			time.sleep(0.01)
+			time.sleep(0.1)
 		except KeyboardInterrupt:
 			input('Please press enter to continue:')
 			end = time.time() + remaining
@@ -57,6 +57,8 @@ warmup_time = config['TIMBER']['warmup_time_sec']
 rest_time = config['TIMBER']['rest_time_sec']
 interval_time = config['TIMBER']['interval_time_sec']
 number_intervals = config['TIMBER']['number_intervals']
+status_url = Request('https://raw.githubusercontent.com/davidemerson/timber/main/timber.txt', 
+              headers={'User-Agent': 'Mozilla/5.0'})
 
 ## sync clocks
 sync()
@@ -66,14 +68,26 @@ print(warmup_time," seconds of warmup.")
 print(rest_time," seconds of rest between intervals.")
 print(interval_time," seconds of exercise each interval.")
 print(number_intervals," intervals.")
+print(status_url)
 input('Please press enter to continue:')
 interval_count = 0
 rest_count = 0
 
 ## work out!
 while True:
-	url = "http://timber.nnix.com/timber.json"
-	response = requests.get(url, headers=headers)
+	try:
+		response = urlopen(status_url).read()
+		print(response)
+		time.sleep(.1)
+		if response == b'idle':
+			print("waiting to start...")
+			continue
+		if response == b'go':
+			break
+	except:
+		input("Didn't get a valid server response. Press any key.")
+		break
+
 interval(warmup_time,0)
 while int(interval_count) < int(number_intervals):
 	interval(rest_time,2)
