0dbcdbed1bbe09f8f14243baf88c68190f680b64
David Emerson <d@nnix.com>
2023-02-10 21:06:24 -0500

    externalized configuration

--- timber.go
+++ timber.go
@@ -4,61 +4,108 @@ import (
     "bufio"
     "fmt"
     "net"
+    "os"
+    "strconv"
     "strings"
     "time"
 )
 
-const (
-    workInterval = 20 * time.Second
-    restInterval = 10 * time.Second
-)
+type Config struct {
+    port     int
+    duration int
+    interval int
+}
 
 func main() {
-    listener, err := net.Listen("tcp", ":8080")
+    config := loadConfig("config.txt")
+    listener, err := net.Listen("tcp", ":"+strconv.Itoa(config.port))
     if err != nil {
         fmt.Println("Error starting server:", err)
         return
     }
     defer listener.Close()
-    fmt.Println("Server started on port 8080")
-
+    fmt.Println("Server is listening on port", config.port)
+    clients := make(map[net.Conn]string)
     for {
         conn, err := listener.Accept()
         if err != nil {
             fmt.Println("Error accepting connection:", err)
             continue
         }
-        go handleConnection(conn)
+        go handleConnection(conn, clients, config.duration, config.interval)
     }
 }
 
-func handleConnection(conn net.Conn) {
-    defer conn.Close()
+func loadConfig(filename string) Config {
+    file, err := os.Open(filename)
+    if err != nil {
+        fmt.Println("Error opening config file:", err)
+        os.Exit(1)
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+    var config Config
+    for scanner.Scan() {
+        line := scanner.Text()
+        parts := strings.Split(line, "=")
+        key := parts[0]
+        value := parts[1]
+        switch key {
+        case "port":
+            port, err := strconv.Atoi(value)
+            if err != nil {
+                fmt.Println("Error parsing port:", err)
+                os.Exit(1)
+            }
+            config.port = port
+        case "duration":
+            duration, err := strconv.Atoi(value)
+            if err != nil {
+                fmt.Println("Error parsing duration:", err)
+                os.Exit(1)
+            }
+            config.duration = duration
+        case "interval":
+            interval, err := strconv.Atoi(value)
+            if err != nil {
+                fmt.Println("Error parsing interval:", err)
+                os.Exit(1)
+            }
+            config.interval = interval
+        }
+    }
+    return config
+}
+
+func handleConnection(conn net.Conn, clients map[net.Conn]string, duration int, interval int) {
     username := readUsername(conn)
+    clients[conn] = username
     fmt.Println("Accepted connection from", username)
-    startTimer(conn, username)
-}
-
-func readUsername(conn net.Conn) string {
-    reader := bufio.NewReader(conn)
-    fmt.Fprint(conn, "Enter your username: ")
-    username, _ := reader.ReadString('\n')
-    username = strings.TrimSpace(username)
-    return username
-}
-
-func startTimer(conn net.Conn, username string) {
-    startTime := time.Now().Add(5 * time.Second)
-    timer := time.NewTimer(startTime.Sub(time.Now()))
-    for i := 0; i < 5; i++ {
-        <-timer.C
-        fmt.Fprintln(conn, username, "Work interval started...")
-        time.Sleep(workInterval)
-        fmt.Fprintln(conn, username, "Work interval ended.")
-        fmt.Fprintln(conn, username, "Rest interval started...")
-        time.Sleep(restInterval)
-        fmt.Fprintln(conn, username, "Rest interval ended.")
-        timer.Reset(workInterval + restInterval)
+    for {
+        message, err := bufio.NewReader(conn).ReadString('\n')
+        if err != nil {
+            fmt.Println("Error reading from", username+":", err)
+            delete(clients, conn)
+            conn.Close()
+            break
+        }
+        if message == "start\n" {
+            fmt.Println(username, "has started the timer")
+            start := time.Now()
+            for i := 0; i < duration/interval; i++ {
+                time.Sleep(time.Duration(interval) * time.Second)
+                for client := range clients {
+                    _, err := client.Write([]byte(strconv.Itoa(interval*(i+1)) + "\n"))
+                    if err != nil {
+                        fmt.Println("Error writing to", clients[client]+":", err)
+                        client.Close()
+                        delete(clients, client)
+                    }
+                }
+            }
+            fmt.Println(username, "timer has completed in", time.Since(start))
+        } else {
+            fmt.Println("Received message from", username+":", message)
+        }
     }
-    fmt.Fprintln(conn, username, "Timer ended.")
 }
